ea05724781cee0b9eb4ee1b9eec385ccba8e8732
Stefan Janhunen <sjanhunen@gmail.com>
2012-01-28 00:10:19 -0500

    Prepare for SqLite storage of texts.

--- tools/search.py
+++ tools/search.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python
 
 import sys
+import sqlite3
 
-text = open(sys.argv[1]).read()
-words = text.split()
+words = open(sys.argv[1]).read().split()
+book = 'Eph'
+
+# Create an sqlite database for storing gnt
+#
+# Table: Words (every word in order)
+#   wordId   verseId    word    partOfSpeech    strongsId
+# Table: Verses (every verse in order)
+#   verseId     book    chapter verse   firstWord   lastWord
+# Table: Strongs (every Strong's number in order)
+#   strongsId   TBD
+
+index = 0
 
 w = None
 variation = False
@@ -19,7 +31,7 @@ for word in words:
         variation = True
     elif word.isdigit():
         # Strong's number
-        w['number'].append(int(word))
+        w['strongs'].append(int(word))
     elif word.startswith('{'):
         # Part of speech
         w['pos'] = word.translate(None, '{}')
@@ -28,12 +40,23 @@ for word in words:
         pass
     elif word.find(':') != -1:
         # Verse
-        verse = word.partition(':')
-        print '%s %d:%d' % (sys.argv[1], int(verse[0]), int(verse[2]))
+        (chapter, sep, verse) = word.partition(':')
+        chapter = int(chapter)
+        verse = int(verse)
+        print '%s %d:%d' % (book, chapter, verse)
     elif word.isalpha():
-        # Greek text
+        # Greek word
         if w:
             print w
-        w = {'text': word, 'pos': None, 'number': []}
+        w = {
+            'word': word,
+            'index': index,
+            'book': book,
+            'chapter': chapter,
+            'verse': verse,
+            'pos': None,
+            'strongs': []}
+        index = index + 1
 
-print w
+if w:
+    print w
