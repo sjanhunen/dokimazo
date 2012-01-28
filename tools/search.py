fdd072a51ab9be52541fb1310d5fab77a985ae66
Stefan Janhunen <sjanhunen@gmail.com>
2012-01-27 20:21:04 -0500

    Parsing actually works now.
    Skip over VAR: ... :END. I believe these are indicators for variants.
    Parse a few more cases properly.

--- tools/search.py
+++ tools/search.py
@@ -6,18 +6,34 @@ text = open(sys.argv[1]).read()
 words = text.split()
 
 w = None
+variation = False
 for word in words:
-    verse = word.partition(':')
-    if verse[1] == ':':
+    if variation:
+        # Skip over variants of the text
+        if word == ':END':
+            variation = False
+        continue
+
+    if word == 'VAR:':
+        # Textual variant
+        variation = True
+    elif word.isdigit():
+        # Strong's number
+        w['number'].append(int(word))
+    elif word.startswith('{'):
+        # Part of speech
+        w['pos'] = word.translate(None, '{}')
+    elif word.startswith('('):
+        # Not sure what this is
+        pass
+    elif word.find(':') != -1:
+        # Verse
+        verse = word.partition(':')
         print '%s %d:%d' % (sys.argv[1], int(verse[0]), int(verse[2]))
-    else:
-        if word.isdigit():
-            w['number'].append(int(word))
-        elif word.startswith('{'):
-            w['part'] = word.translate(None, '{}')
-        elif word.isalpha(): 
-            if w:
-                print w
-            w = {'text': word, 'part': None, 'number': []}
+    elif word.isalpha():
+        # Greek text
+        if w:
+            print w
+        w = {'text': word, 'pos': None, 'number': []}
 
 print w
